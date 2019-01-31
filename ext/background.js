1afa37644458ad1ffd980f502651b2cbec66a3d8
wallen <wallen_weel@outlook.com>
2019-01-31 21:43:35 +0800

    feat: implement aria2 jsonrpc message

--- ext/background.js
+++ ext/background.js
@@ -1,1 +1,27 @@
-console.log('background script')
+const JSON_RPC = 'http://localhost:6800/jsonrpc'
+const METHOD = 'POST'
+
+browser.runtime.onMessage.addListener(async ({ payload = {} }) => {
+  const { target = 'aria2', url = JSON_RPC, data = {}, config = {} } = payload
+
+  if (target === 'aria2') await Promise.resolve(await aria2download(url, data, config))
+})
+
+async function aria2download (url, data = {}, config = {}) {
+  return await request(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    ...config
+  })
+}
+
+function request (url, config = {}) {
+  const { timeout } = { timeout: 5000, ...config }
+
+  const controller = new AbortController()
+  const signal = controller.signal
+
+  setTimeout(() => controller.abort(), timeout)
+
+  return fetch(url, { signal, ...config })
+}
--- ext/content.js
+++ ext/content.js
@@ -1,1 +1,11 @@
-console.log('content script')
+window.addEventListener('weel', async ({ detail = {} }) => {
+  const { version = '1.0', type = 'asmr', payload = {} } = detail
+
+  if ('1.0' !== version) return false
+
+  if ('asmr' === type) await send({ type, payload })
+}, false)
+
+function send (message) {
+  return browser.runtime.sendMessage(message)
+}
